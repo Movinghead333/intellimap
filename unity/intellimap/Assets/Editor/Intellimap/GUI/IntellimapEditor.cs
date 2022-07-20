2c1e73573431e7c4926204784160698cb3d07865
Timo Friedrich <timo.friedrich99@gmx.de>
2022-07-20 16:29:24 +0200

    Re-added completing the current run when clicking generate entire map

--- unity/intellimap/Assets/Editor/Intellimap/GUI/IntellimapEditor.cs
+++ unity/intellimap/Assets/Editor/Intellimap/GUI/IntellimapEditor.cs
@@ -209,7 +209,9 @@ public class IntellimapEditor : EditorWindow {
     }
 
     private void GenerateEntireMapButtonPressed() {
-        createNewWFCRun();
+        if (wfc == null) {
+            createNewWFCRun();
+        }
 
         if (wfc != null) {
             wfc.GenerateEntireMap();
